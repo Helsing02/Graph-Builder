9817f3c9217b474e202ba14af25a3a4e498440cc
Бриц Владислав <81351838+OwLRUS@users.noreply.github.com>
2022-12-02 22:30:57 +0700

    Add files via upload

---
+++ FuncCollection.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class FuncCollection
+{
+    vector <Function> arr_function;
+    FuncWindow canvas; 
+public:
+    void AddFunc(string newfunc);
+    void DeleteFunc(string oldfunc);
+    void RecastGraph();
+};
+
+void FuncCollection::AddFunc(string newfunc)
+{
+    Function New(newfunc);
+    arr_function.push_back(New);
+}
+
+void FuncCollection::DeleteFunc(string oldfunc)
+{
+    int i = 0;
+    while (arr_function[i].exp != oldfunc) i++;
+
+    arr_function.erase(arr_function.begin() + i);
+}
+
+void FuncCollection::RecastGraph()
+{
+    canvas.recast();
+}
+
+int main()
+{
+    // я у мамы программист
+}
