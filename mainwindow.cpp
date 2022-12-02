36bd780265c7fe4399396996c448a87e45965a33
ZhenyaShnayder <shnayder02@yandex.ru>
2022-12-02 18:35:03 +0700

    При масштабировании окна будут меняться размеры виджетов

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,13 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 #include <QLabel>
+#include <QColor>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->setStyleSheet("background-color:rgb(255,246,230);");
 }
 
 MainWindow::~MainWindow()
@@ -14,3 +16,7 @@ MainWindow::~MainWindow()
 }
 
 
+void MainWindow::on_addBtn_clicked()
+{
+
+}
