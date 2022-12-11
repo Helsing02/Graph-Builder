2c302b1f3d2beaa21a50a932099f8e25604760d7
OwLRUS <vladislav.bric@mail.ru>
2022-12-11 20:07:37 +0700

    Изменил по ДК

--- FuncWindow.h
+++ FuncWindow.h
@@ -4,25 +4,26 @@
 #include <QMainWindow>
 #include "qcustomplot.h"
 
-namespace Ui {
+namespace Ui
+{
 class FuncWindow;
 }
 
 class FuncWindow : public QMainWindow
 {
     Q_OBJECT
-
 public:
+
     explicit FuncWindow(QWidget *parent = nullptr);
     ~FuncWindow();
-    void addGraph(QVector <double>, QVector <double>, int);
-    void changeSize(int, int, int, int);
-    void replot();
-    void clearGraphs();
-
+    int add_func(std::string new_func);
+    void add_graphs(int x_min, int x_max);
+    void change_size(int, int, int, int);
 private:
+
     Ui::FuncWindow *ui;
-    QCustomPlot *wGraphic;
+    QCustomPlot *w_graphic;
+    void rebuild();
 };
 
 #endif // FUNCWINDOW_H
