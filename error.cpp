c66f688177f9ccb08daea473fd6d0fc252bf3742
ZhenyaShnayder <shnayder02@yandex.ru>
2022-12-24 23:36:20 +0700

    1.9 Почти

--- error.cpp
+++ error.cpp
@@ -17,13 +17,7 @@ error::~error()
 
 void error::erro(int i){
     if(i==1)
-        ui->text->setText("Ошибка связана со скобками ");
-    else if(i==2)
-        ui->text->setText("Некорректно введеные данные ");
-    else if(i==3)
-        ui->text->setText("Ошибка связана с некорректность введных операций ");
-    else if(i==4)
-        ui->text->setText("Строка не заполнена ");
+        ui->text->setText("Некорректно введена формула ");
     else if(i==5)
         ui->text->setText("Посторонний символ при задании интервала");
     else if(i==6)
